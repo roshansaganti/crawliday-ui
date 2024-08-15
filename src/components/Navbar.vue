52c3bff100027960ccaee6708d5bfc20a420ff15
Roshan Saganti <roshansaganti@hotmail.com>
2024-08-15 19:43:03 -0400

    Add main content
    - Removed unused dropdown button from Navbar
    - Removed unused HelloWorld.vue component
    - Removed unused Vue logo.png

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
     <b-container>
       <b-navbar-brand href="#">Crawliday</b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <!-- <b-navbar-toggle target="nav-collapse"></b-navbar-toggle> -->
 
       <!-- <b-collapse id="nav-collapse" is-nav></b-collapse> -->
     </b-container>
